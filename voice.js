5d6a4412aa33050f3a9c830f9ef747cb65a0c691
V. Aniketh <52618459+aniketh008@users.noreply.github.com>
2022-02-08 12:56:42 +0530

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,5 +1,4 @@
 const btn = document.getElementById('btn');
-import { getWeather } from "./api";
 
 const SpeechRecognition = window.SpeechRecognition ||  window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -22,18 +21,13 @@ function read(text){
     if(text.includes('Hello ')) 
     speech.text = "Hello Aniketh, How are you doing today";
 
-    if(text.includes('What is your Name')) 
+    else if(text.includes('What is your Name')) 
     speech.text = "My name is Jarvis Your Personal Assitant";
 
-    else if(text.includes('what is time'))
+    else if(text.includes('time'))
     speech.text = 'It is' + new Date().getHours() + "" + new Date().getMinutes()+" right now"
 
-    else if(text.includes('Weather ')) 
-    const location = "Hyderabad";
-    const Weather = await getWeather(location);
-    speech.text`The weather for today in ${location} is ${weather} degrees`;
-
-    if(text.includes('My Birthday')) 
+    else if(text.includes('My Birthday')) 
     speech.text = "8th September 2001";
 
     window.speechSynthesis.speak(speech);
