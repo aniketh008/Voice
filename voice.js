4e5a3a8fb44f0957ddb590a15fc915415d4778d6
V. Aniketh <52618459+aniketh008@users.noreply.github.com>
2022-02-08 13:43:51 +0530

    Update voice.js

--- voice.js
+++ voice.js
@@ -31,8 +31,6 @@ function read(text){
     else if(text.includes('what is your name')) 
     speech.text = 'My name is Jarvis Your Personal Assistant';
     
-    else if(text.includes('open youtube')) 
-        window.open('https://www.youtube.com/')
     
     window.speechSynthesis.speak(speech);
 }
